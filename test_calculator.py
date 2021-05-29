18c7371574259f632d18771af92df16107cdd9ec
Timothy Nguyen <timothynguyen2000@yahoo.com>
2021-05-29 15:03:01 -0700

    added tests

---
+++ test_calculator.py
@@ -0,0 +1,19 @@
+"""
+Tests for calc app
+"""
+import calculator
+
+
+class TestCalculatorApp:
+    def test_add(self):
+        assert 5 == calculator.add(3,2)
+    
+    def test_subtract(self):
+        assert 5 == calculator.subtract(10,5)
+
+    def test_multiply(self):
+        assert 15 == calculator.multiply(3,5)
+
+    def test_divide(self):
+        assert 3 == calculator.divide(15,5)
+        
