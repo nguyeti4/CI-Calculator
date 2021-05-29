5f4689f6492f29caff1ef147ec933e4331524b37
Timothy Nguyen <timothynguyen2000@yahoo.com>
2021-05-29 15:18:07 -0700

    fixed syntax

--- calculator.py
+++ calculator.py
@@ -2,14 +2,18 @@
 calculator app using circle CI
 """
 
-def add(a,b):
-        return a + b
 
-def subtract(a,b):
-        return a - b
+def add(a, b):
+    return a + b
 
-def multiply(a,b):
-        return a * b
 
-def divide(a,b):
-        return a / b
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    return a / b
